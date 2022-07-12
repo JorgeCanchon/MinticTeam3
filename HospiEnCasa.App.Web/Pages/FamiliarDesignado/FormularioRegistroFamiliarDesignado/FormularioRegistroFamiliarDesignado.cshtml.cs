b348f7f0d99bb0f166dc41e87d54a44ed333e340
CarlosIntiGarcia <carlosgarciamvz@hotmail.com>
2022-07-11 19:43:57 -0500

    Cambio de formulario de registro familiar designado, creacion de formulario de registro paciente y medico

---
+++ HospiEnCasa.App.Web/Pages/FamiliarDesignado/FormularioRegistroFamiliarDesignado/FormularioRegistroFamiliarDesignado.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospiEnCasa.App.Persistencia;
+using HospiEnCasa.App.Dominio;
+
+namespace HospiEnCasa.App.Web.Pages
+{
+    public class FormularioRegistroFamiliarDesignadoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
