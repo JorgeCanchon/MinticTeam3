9a57e16290a1e170bc12fcbb3ab377dbc6beb5a4
CarlosIntiGarcia <carlosgarciamvz@hotmail.com>
2022-07-26 10:12:13 -0500

    Notacion de esquema agregado a entidades Paciente y Persona

--- HospiEnCasa.App.Dominio/Entidades/Paciente.cs
+++ HospiEnCasa.App.Dominio/Entidades/Paciente.cs
@@ -1,14 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HospiEnCasa.App.Dominio
 {
     public class Paciente : Persona
     {
+        [ForeignKey("Id")]
         public Historia Historia { get; set; }
+        [ForeignKey("Id")]
         public List<SignoVital> SignoVitales { get; set; }
+        [ForeignKey("Id")]
         public FamiliarDesignado FamiliarDesignado { get; set; }
+        [ForeignKey("Id")]
         public Enfermera Enfermera { get; set; }
+        [ForeignKey("Id")]
         public Medico Medico { get; set; }
         public string Direccion { get; set; }
         public float? Latitud { get; set; }
